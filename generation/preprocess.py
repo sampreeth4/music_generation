805b994edfc795e71907e81f9d3fd47d17ed6919
sampreeth <saisampreeth78@gmail.com>
2025-04-13 18:37:12 +0530

    added structure

---
+++ generation/preprocess.py
@@ -0,0 +1,38 @@
+"""
+1- load a file
+2- pad the signal (if necessary)
+3- extracting log spectrogram from signal
+4- normalise spectrogram
+5- save the normalised spectrogram
+
+PreprocessingPipeline
+"""
+
+import os
+import pickle
+import librosa
+import numpy as np
+
+class Loader:
+    "loads audio files"
+
+    def __init__(self):
+        pass
+
+class Padder:
+    pass
+
+
+class LogSpectrogramExtractor:
+    pass
+
+
+class MinMaxNormaliser:
+    pass
+
+
+class Saver:
+    pass
+
+class PreprocessingPipeline:
+    pass
